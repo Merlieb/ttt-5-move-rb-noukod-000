338187559363fc7e30d2af72343ca3d269e4803a
Merline Belizaire <merline@noukod.com>
2018-08-30 19:31:25 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,9 +6,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def display_board()
-
-end
 def input_to_index(user_input)
 #convert string
   ma = user_input.to_i
